f276ef4e834f74d01379930d04ee73c552cb6cfa
ctakasaka <ctakasaka@outlook.com>
2020-09-18 16:33:24 -0700

    Adding a cpp file that may build

---
+++ input_reader.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string userString; // will hold initial user input
+
+    cout << "Type something in: "; // ask user for any input
+
+    cin >> userString; // store user input in the given variable
+    
+    cout << "Your string is: " << userString;
+
+    return 0;
+}
