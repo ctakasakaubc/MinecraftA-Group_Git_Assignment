c6ea65befae9586039c2f7d7513983e1f9a5573d
ctakasaka <ctakasaka@outlook.com>
2020-09-18 17:32:48 -0700

    Updated skeleton code

--- input_reader.cpp
+++ input_reader.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main() {
 
     string userString; // will hold initial user input
-    int userNumber = 723234; // this will be used if the user has an integer in their input (random int to reduce likelihood of user choosing it and bugging)
+    int userNumber = -1; // this will be used if the user has an integer in their input (random int to reduce likelihood of user choosing it and bugging)
+    bool isInt = false;
 
     cout << "Type something in: "; // ask user for any single word or number input
     cin >> userString; // store user input in the given variable
@@ -15,11 +16,16 @@ int main() {
         int testNum = std::stoi(userString); // try to parse some integer from the user's string
         if(to_string(userNumber).length() == userString.length()){
             userNumber = testNum;
+            isInt = 1;
         }
     }catch(...){}
 
-    // if userNumber is still 723234, the input was just some string
-    // else it is the integer that was parsed from their string, and it is the same length as their string
-    
+    // isInt will be true if the user's input was an integer
+    if(isInt){
+        enteredInteger(); // called if the input is an integer
+    }else{
+        enteredString(); // called if the input is a string
+    }
+
     return 0;
 }
